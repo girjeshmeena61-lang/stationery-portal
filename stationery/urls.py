4211bf0c88a81532a2306c41e8e354d5d7b77ffe
girjesh meena <girjeshmeena@girjeshs-MacBook-Air.local>
2026-07-01 21:14:34 +0530

    Restore portal views and custom login

--- stationery/urls.py
+++ stationery/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.requisition_form, name='requisition_form'),
+    path('', views.create_requisition, name='create_requisition'),
     path('status/', views.request_status, name='request_status'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
