478821a6e1261901767cc4f2a919c7d63296dccf
girjesh meena <girjeshmeena@girjeshs-MacBook-Air.local>
2026-07-01 20:39:31 +0530

    Fix urls configuration

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from stationery.views import custom_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # Custom login page with User/Admin dropdown
     path('accounts/login/', custom_login, name='login'),
 
+    # Logout
     path(
         'accounts/logout/',
         LogoutView.as_view(),
         name='logout'
     ),
 
+    # Stationery application URLs
     path('', include('stationery.urls')),
 ]
